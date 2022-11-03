1546a18b67d9628ce3ed8bc9c8f9f632d305d175
BradleyGCF <bradleycarucci@gmail.com>
2022-11-02 22:23:20 -0400

    primer dia del front

---
+++ client/src/store/index.js
@@ -0,0 +1,7 @@
+import { configureStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import rootReducer from "../reducer";
+
+
+export const store = configureStore
